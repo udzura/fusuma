714b9a1493d910aa1fa10d34bbfca16c6e12f239
Russel Hunter Yukawa <rhykw@users.noreply.github.com>
2017-07-28 00:29:54 +0900

    Fix ci (#1)
    To fix CI

---
+++ .travis_build_config.rb
@@ -0,0 +1,29 @@
+def gem_config(conf)
+  #conf.gembox 'default'
+
+  # be sure to include this gem (the cli app)
+  conf.gem File.expand_path(File.dirname(__FILE__))
+end
+
+MRuby::Build.new do |conf|
+  if `uname` =~ /linux/i
+    toolchain :gcc
+  else
+    toolchain :clang
+  end
+
+  conf.enable_bintest
+  conf.enable_debug
+  conf.enable_test
+
+  conf.gembox 'default'
+
+  conf.gem :github => 'iij/mruby-mtest'
+  gem_config(conf)
+end
+
+# MRuby::Build.new('x86_64-pc-linux-gnu') do |conf|
+#   toolchain :gcc
+
+#   gem_config(conf)
+# end
